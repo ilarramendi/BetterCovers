f6aebdd78bdb29b5d9595890e01b8ec9255755af
ilarramendi <ilarramendi1@gmail.com>
2021-03-18 19:07:19 -0400

    Changed text color and added transparency (r/5w7nX3Y4ghpVdK5o suggestion)

--- EmbyCoverRatings.py
+++ EmbyCoverRatings.py
@@ -9,8 +9,9 @@ import sys
 
 brHeight = 40
 imHeight = 30
-txHeight = 25
-
+txHeight = 26
+rectangleColor = (25, 25, 25, int(255 * 0.8))
+textColor = '#fcfcfc'
 extensions = ['mp4', 'mkv']
 
 with open('./config.json', 'r') as json_file:
@@ -46,10 +47,9 @@ for file in files:
         
         response = requests.get('http://www.omdbapi.com/?apikey=' + apiKey + '&t=' + name.replace(' ', '+'))
         res = response.json()
-        if response.status_code != 200 or 'Error' in res: print(name, 'No movies found')
+        if response.status_code != 200 or 'Error' in res: print('No movies found: ' + name)
         else:
-            yr = int(re.findall('\d{4}' ,res['Year'])[0])
-            if yr > int(year) + 1 or yr < int(year) - 1: print('Wrong movie found', name, res['Title'], year, yr)
+            if abs(int(year) - int(re.findall('\d{4}' ,res['Year'])[0])) > 1: print('Wrong movie found: ' + name + ' (' + year + ') | ' + res['Title'] + ' (' + res['Year'] + ')')
             else:  
                 if 'Metascore' in res: info['mts'] = res['Metascore']
                 if 'imdbRating' in res: info['imdb'] = res['imdbRating']
@@ -63,23 +63,25 @@ for file in files:
                     if info[inf] != 'N/A': info2[inf] = info[inf]
 
                 if len(info2) > 0 and 'Poster' in res and downloadImage(res['Poster'], 'cover.jpg', 3):
-                    img = Image.open("cover.jpg")
-                    draw = ImageDraw.Draw(img)
-                    draw.line((0, img.size[1]) + img.size, fill='#505059', width=brHeight * 2)
+                    img = Image.open("cover.jpg").convert("RGBA")
+                    overlay = Image.new('RGBA', img.size, (0,0,0,0))
+                    ImageDraw.Draw(overlay).rectangle(((0, img.size[1] - brHeight), (img.size[0], img.size[1])), fill=rectangleColor)
+                    img = Image.alpha_composite(img, overlay).convert("RGB") # Remove alpha for saving in jpg format.
+
                     font = ImageFont.truetype("Roboto-Medium.ttf", txHeight)
                     le = 300 // len(info2)
                     x = 0
-                    space = 10 // len(info2)
+                    space = 15 // len(info2)
+                    draw = ImageDraw.Draw(img)
                     for rt in info2:
                         im = Image.open(rt + ".png")
                         tsize = draw.textsize(info2[rt], font)[0]
                         sp  = (le - im.size[0] - tsize - space) // 2
                         img.paste(im, (x + sp, img.size[1] - imHeight - (brHeight - imHeight) // 2), im)
-                        draw.text((x + sp + im.size[0] + space, img.size[1] - txHeight - (brHeight - txHeight) // 2), info2[rt],(0,0,0),font=font)
+                        draw.text((x + sp + im.size[0] + space, img.size[1] - txHeight - (brHeight - txHeight) // 2 - 1) , info2[rt], textColor, font=font)
                         x += le
 
                     img.save(file.rpartition('/')[0] + '/cover.jpg')
-                    print(res['Title'] + ' Cover Saved!')
-                else: print(res['Title'] + ' No ratings found or poster missing.')
-
+                    print('\033[92m' + 'Cover image saved for: ' + res['Title'] + '\033[0m')
+                else: print('No ratings found or poster missing for movie: ' + res['Title'])
     
