1a1ceb071b7ee21c6b6f8cb615ffd7ac1e0546ac
ilarramendi <ilarramendi1@gmail.com>
2021-03-20 01:25:00 -0400

    Fixed bugs related to media info, added color to error messages.

--- CoverRatings.py
+++ CoverRatings.py
@@ -48,7 +48,11 @@ def downloadImage(url, path, retry):
         logText('Failed to download: ' + url, error = True)
         return False
 
-for file in glob([pt for pt in sys.argv[1:] if '/' in pt][0]):
+i = 1
+files = sorted(glob([pt for pt in sys.argv[1:] if '/' in pt][0]))
+for file in files:
+    print('[' + str(i) + '/' + str(len(files)) +'] ', end='')
+    i += 1
     if path.exists(file + '/poster.jpg') and not overWrite: # Skip if cover exists
         print('Cover already exists and overwrite is disabled')
         continue 
@@ -70,10 +74,10 @@ for file in glob([pt for pt in sys.argv[1:] if '/' in pt][0]):
     response = get('http://www.omdbapi.com/?apikey=' + apiKey + '&tomatoes=true&t=' + name.replace(' ', '+') + ('&y=' + year if year else ''))
     res = response.json()
     if response.status_code != 200 or 'Error' in res: 
-        print('No info found for:', name, year)
+        print('\033[93mNo info found for:', name, year, '\033[0m')
         continue
     if year and abs(int(year) - int(findall('\d{4}' ,res['Year'])[0])) > 1: 
-        print('Wrong info found: ' + name + ' (' + year + ') | ' + res['Title'] + ' (' + res['Year'] + ')')
+        print('\033[93mWrong info found: ' + name + ' (' + year + ') | ' + res['Title'] + ' (' + res['Year'] + ')\033[0m')
         continue
     # endregion
     
@@ -84,39 +88,42 @@ for file in glob([pt for pt in sys.argv[1:] if '/' in pt][0]):
     if len(rts) > 0 and rts[0]['Value'] != 'N/A': info['rt'] = str(int(rts[0]['Value'][:-1]) / 10)
     infoIndex = list(filter(lambda inf: info[inf] != 'N/A', info)) # Filter ratings
     if 'mts' in infoIndex: info['mts'] = str(int(info['mts']) / 10)
-    if len(infoIndex) == 0:
-        print('No ratings found for:', res['Title'])
+    if len(infoIndex) == 0 and not mediaInfo:
+        print('\033[93mNo ratings found and mediaInfo not set for:', res['Title'], '\033[0m')
         continue
     infoIndex.sort()
     # endregion
 
     # region Download image
     if not ('Poster' in res and res['Poster'] != 'N/A' and downloadImage(res['Poster'], 'cover.jpg', 3)):
-        print('Poster missing or failed to download for: ' + res['Title'])
+        print('\033[93mPoster missing or failed to download for:', res['Title'], '\033[0m')
         continue
     # endregion
 
     # region Add ratings to images
-    img = Image.open("./cover.jpg").convert("RGBA").resize((300,450))
-    overlay = Image.new('RGBA', img.size, (0,0,0,0))
-    recsize = ((0, img.size[1] - brHeight if vAlign == 'b' else 0), (img.size[0], img.size[1] if vAlign == 'b' else brHeight)) 
-    ImageDraw.Draw(overlay).rectangle(recsize, fill=rectangleColor)
-    img = Image.alpha_composite(img, overlay).convert("RGB")
-    font = ImageFont.truetype(resource_path("media/font.ttf"), txHeight)
-    draw = ImageDraw.Draw(img)
+    
+    if len(infoIndex) > 0:
+        img = Image.open("./cover.jpg").convert("RGBA").resize((300,450))
+        overlay = Image.new('RGBA', img.size, (0,0,0,0))
+        recsize = ((0, img.size[1] - brHeight if vAlign == 'b' else 0), (img.size[0], img.size[1] if vAlign == 'b' else brHeight)) 
+        ImageDraw.Draw(overlay).rectangle(recsize, fill=rectangleColor)
+        img = Image.alpha_composite(img, overlay).convert("RGB")
+        font = ImageFont.truetype(resource_path("media/font.ttf"), txHeight)
+        draw = ImageDraw.Draw(img)
 
-    le = (300 - padding) // len(infoIndex) 
-    x = padding if hAlign == 'c' else 300 if hAlign == 'r' else 0
-    space = 3
-    for rt in infoIndex if hAlign != 'r' else reversed(infoIndex):
-        im = Image.open(resource_path('media/' + rt + ".png")) 
-        tsize = draw.textsize(info[rt], font)[0]
-        sp = (le - im.size[0] - tsize - space) // 2
-        imgPos = (x + sp if hAlign == 'c' else (x + space) if hAlign == 'l' else x - tsize - space * 2 - im.size[0], (img.size[1] - imHeight - (brHeight - imHeight) // 2) if vAlign == 'b' else (brHeight - imHeight) // 2)
-        img.paste(im, imgPos, im)
-        txtPos = (x + sp + im.size[0] + space if hAlign == 'c' else x + space * 2 + im.size[0] if hAlign == 'l' else imgPos[0] + im.size[0] + space, img.size[1] - txHeight - (brHeight - txHeight) // 2 - 1 if vAlign == 'b' else (brHeight - txHeight) // 2 - 1)
-        draw.text(txtPos, info[rt], textColor, font=font)
-        x = x + le if hAlign == 'c' else txtPos[0] + tsize if hAlign == 'l' else imgPos[0]
+        le = (300 - padding) // len(infoIndex) 
+        x = padding if hAlign == 'c' else 300 if hAlign == 'r' else 0
+        space = 3
+        for rt in infoIndex if hAlign != 'r' else reversed(infoIndex):
+            im = Image.open(resource_path('media/' + rt + ".png")) 
+            tsize = draw.textsize(info[rt], font)[0]
+            sp = (le - im.size[0] - tsize - space) // 2
+            imgPos = (x + sp if hAlign == 'c' else (x + space) if hAlign == 'l' else x - tsize - space * 2 - im.size[0], (img.size[1] - imHeight - (brHeight - imHeight) // 2) if vAlign == 'b' else (brHeight - imHeight) // 2)
+            img.paste(im, imgPos, im)
+            txtPos = (x + sp + im.size[0] + space if hAlign == 'c' else x + space * 2 + im.size[0] if hAlign == 'l' else imgPos[0] + im.size[0] + space, img.size[1] - txHeight - (brHeight - txHeight) // 2 - 1 if vAlign == 'b' else (brHeight - txHeight) // 2 - 1)
+            draw.text(txtPos, info[rt], textColor, font=font)
+            x = x + le if hAlign == 'c' else txtPos[0] + tsize if hAlign == 'l' else imgPos[0]
+    else: img = Image.open("./cover.jpg").resize((300,450))
     # endregion
     
     # region Add mediaInfo to images
@@ -124,11 +131,11 @@ for file in glob([pt for pt in sys.argv[1:] if '/' in pt][0]):
         mediaFiles = []
         for ex in ['mkv', 'mp4']: mediaFiles += glob(file + '/*.' + ex)
         if len(mediaFiles) == 1:
-            out = getstatusoutput('mediainfo "' + mediaFiles[0] + '" --Inform="Video;%colour_primaries%,%Height%,%Format%"')
+            out = getstatusoutput('mediainfo "' + mediaFiles[0] + '" --Inform="Video;%colour_primaries%,%Height%,%Width%,%Format%"')
             if out[0] == 0:
                 x = img.size[0] - space
-                y = brHeight + 2 if vAlign == 't' else 5
-                if '2160' in out[1]:
+                y = brHeight + 2 if vAlign == 't' and len(infoIndex) > 0 else 5
+                if '2160' in out[1] or '3840' in out[1]:
                     im = Image.open(resource_path('media/4k.png'))
                     img.paste(im, (x - im.size[0], y), im)
                     x -= im.size[0] + space
@@ -140,7 +147,7 @@ for file in glob([pt for pt in sys.argv[1:] if '/' in pt][0]):
                     im = Image.open(resource_path('media/hdr.png'))
                     img.paste(im, (x - im.size[0], y), im)
             else: print('Error getting media info, Is mediainfo installed?', out[1])
-        else: print('No media files found')
+        else: print('No media files found, skipping MediaInfo for:', res['Title'])
     # endregion
     
     img.save(file + '/poster.jpg')
